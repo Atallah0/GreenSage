e435b9b071cd8cb7e9f46e9a4376465f07724e11
Atallah <Atallah.192000@gmail.com>
2024-01-12 01:27:54 +0200

    delete an Item from a cart deleted

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -291,6 +291,64 @@ const removeItemFromCart = asyncWrapper(async (req, res, next) => {
     }
 });
 
+// // removeProductFromCart Endpoint/API
+const removeProductFromCart = asyncWrapper(async (req, res, next) => {
+    const userId = req.params.userId;
+    const productId = req.params.productId;
+
+    // Check if the userID is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return next(createCustomError(`Invalid user ID: ${userId}`, 400));
+    }
+
+    // Check if the productId is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+        return next(createCustomError(`Invalid user ID: ${productId}`, 400));
+    }
+
+    // logging the process
+    console.log('Removing Item from Cart with userId: ' + userId);
+
+    // fetch the cart and product
+    const cart = await Cart.findOne({ userId });
+    const productToRemove = await Product.findById(productId);
+
+    // If the product to remove does not exist
+    if (!productToRemove) {
+        return next(createCustomError(`Product does not exist`, 404));
+    }
+
+    // Check if the cart contains the product
+    const existingCartItem = cart.cartItems.find(item => item.productId.equals(productId));
+
+    if (existingCartItem) {
+        // If product quantity is 1 or no quantity is provided, remove the entire product
+        const newTotal = parseFloat(cart.totalPrice) - existingCartItem.price * existingCartItem.quantity;
+
+        // Remove the cart item from the Product model's cartItems array
+        await Product.findByIdAndUpdate(productId, {
+            $pull: { cartItems: existingCartItem._id },
+        });
+
+        await Cart.findByIdAndUpdate(cart._id, {
+            totalPrice: newTotal,
+            $pull: { cartItems: { productId: existingCartItem.productId } },
+            totalItems: cart.totalItems - 1
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: `Item Removed from Cart Successfully`,
+            data: existingCartItem,
+        });
+    } else {
+        return res.status(200).json({
+            success: false,
+            message: 'Item not found in the cart',
+        });
+    }
+});
+
 // clearCart Endpoint/API
 const clearCart = asyncWrapper(async (req, res, next) => {
     const userId = req.params.userId;
@@ -339,5 +397,6 @@ module.exports = {
     fetchCart,
     addItemToCart,
     removeItemFromCart,
-    clearCart
+    removeProductFromCart,
+    clearCart,
 }
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -5,12 +5,14 @@ const {
     fetchCart,
     addItemToCart,
     removeItemFromCart,
+    removeProductFromCart,
     clearCart
 } = require('../controllers/cartController')
 
 router.get('/:id', fetchCart);
 router.post('/:userId/items/:productId', addItemToCart);
 router.delete('/:userId/items/:productId', removeItemFromCart);
+router.delete('/:userId/products/:productId', removeProductFromCart);
 router.delete('/:userId', clearCart);
 
 module.exports = router;
